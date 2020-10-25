28a3ea4a275f42a538bc7695f257d9a76a864c03
Ryuchen <chenhaom1993@hotmail.com>
2020-10-25 18:08:24 +0800

    update

--- themes/modify_of_chunky_poster/src/main.js
+++ themes/modify_of_chunky_poster/src/main.js
@@ -1,4 +1,5 @@
 import './scss/styles.scss';
+import Plyr from 'plyr';
 
 import(/* webpackChunkName: "bootstrap" */ 'bootstrap');
 
@@ -13,3 +14,29 @@ window.addEventListener('DOMContentLoaded', async (event) => {
     App.renderProject();
     App.syntaxHighlight();
 });
+
+// Change "{}" to your options:
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('#music-player', {});
+
+player.source = {
+    type: 'audio',
+    title: '我喜欢的音乐',
+    sources: [
+        {
+            src: 'https://cdn.jsdelivr.net/gh/Ryuchen/ryuchen.github.io@master/music/夏天的风.mp3',
+            type: 'audio/mp3',
+        },
+        {
+            src: 'https://cdn.jsdelivr.net/gh/Ryuchen/ryuchen.github.io@master/music/Love-is-gone.mp3',
+            type: 'audio/mp3',
+        }
+    ],
+};
+
+player.volume = 0.35;
+
+player.controls = ['play', 'progress', 'current-time', 'mute', 'volume'];
+
+// Expose player so it can be used from the console
+window.player = player;
